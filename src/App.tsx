c80732288af91f42ace415ca71ecf8960f81e89e
fdutrac <fabiohenrique@gestaointeligentesofware.com.br>
2021-05-14 21:39:54 -0300

    Commit inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useCallback } from "react";
+import api from './services/api';
+import ContentLoader from './ContentLoader';
 
-function App() {
+const App: React.FC = () => {
+  const [photo, setPhoto ] = useState<string>('');
+  const [isLoad, setIsLoad ] = useState<boolean>(false);
+
+  const handleSortImg: any = useCallback(() => {
+    console.log(photo)
+    setIsLoad(true);
+    api.get('').then(
+      response => {
+        setPhoto(response.data.message)
+        setIsLoad(false);
+      }
+    )
+  }, [photo])
+
+  useEffect(() => {
+    setIsLoad(true);
+    api.get('').then(
+      response => {
+        setPhoto(response.data.message);
+        setIsLoad(false);
+      }
+    )
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>Hello Gama Academy</h1>
+        <h4>Veja esse macaquinho:</h4>
+        <button onClick={() => handleSortImg()}>Clique</button>
+      </div>
+      <img width="200px" height="auto" src={photo} alt="dog" />
+      { isLoad && (<ContentLoader/>)}
     </div>
-  );
+    )
 }
 
 export default App;
---
+++ src/ContentLoader.tsx
@@ -0,0 +1,11 @@
+// import loadImg from './img/macaquinho.jpg'
+
+const ContentLoader: React.FC = () => {
+    return (
+        <div>
+            <p>Carregando...</p>
+        </div>
+    )
+}
+
+export default ContentLoader
